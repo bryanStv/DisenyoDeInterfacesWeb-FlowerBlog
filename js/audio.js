3ae2997425740635c3c5c75c6194a2ee8e2092a1
bryanStv <bryans.oyola@gmail.com>
2024-10-19 12:28:59 +0200

    Añadido JS para reproducir y cambiar entre 2 audios y controlar que el audio del video y el audio no se mezclen

---
+++ js/audio.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+    const video  = document.getElementById("videoFlores");
+    const audio1 = document.getElementById("audio1");
+    const audio2 = document.getElementById("audio2");
+
+    alert('Hola')
+
+    function reproducirEnBucle() {
+        audio1.play();
+        
+        audio1.addEventListener('ended', function() {
+            if (audio2 != null && audio2.canPlayType&&audio2.canPlayType("audio/mpeg")){
+                    audio2.play();
+            }
+        });
+        
+        audio2.addEventListener('ended', function() {
+            if (audio1 != null && audio1.canPlayType&&audio1.canPlayType("audio/mpeg")){
+                audio1.play();
+            }
+        });
+    }
+
+    reproducirEnBucle();
+
+    video.addEventListener('play', function() {
+        audio1.pause();
+        audio2.pause();
+    });
+
+    video.addEventListener('pause', function() {
+        reproducirEnBucle();
+    });
+}
